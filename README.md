c37eae28884fd8e931de49dfb988222c705861fc
JasonLei <jlei@activision.com>
2024-02-24 22:14:32 -0800

    add selection border

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # web603-hw-w7d2
+
+## Test
+
+### App Home
+![app-home](screenshots/app-home.png)
+
+### Clicking on App Icons
+
+Clicking on Netflix:
+![click-netflix](screenshots/click-netflix.png)
+
+Clicking on HBO Max:
+![click-netflix](screenshots/click-netflix.png)
+
+Clicking on Hulu:
+![click-netflix](screenshots/click-netflix.png)
+
+Clicking on Prime Video:
+![click-netflix](screenshots/click-netflix.png)
--- router-params/src/App.js
+++ router-params/src/App.js
@@ -1,26 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link, useParams } from 'react-router-dom';
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [selectedApp, setSelectedApp] = useState(null);
+
+  const handleSelectApp = (id) => {
+    setSelectedApp(id);
+  };
+
   return (
     <Router>
       <div>
         <h1 className="title">TV APPS</h1>
         <div className="icon-container">
-          <Link to="/netflix">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/6/69/Netflix_logo.svg" alt="Netflix" className="app-icon" />
-          </Link>
-          <Link to="/hbo">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/1/17/HBO_Max_Logo.svg" alt="HBO Max" className="app-icon" />
-          </Link>
-          <Link to="/hulu">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/e/e4/Hulu_Logo.svg" alt="Hulu" className="app-icon" />
-          </Link>
-          <Link to="/prime">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/f/f1/Prime_Video.png" alt="Prime Video" className="app-icon" />
-          </Link>
+          <div onClick={() => handleSelectApp('netflix')}>
+            <Link to="/netflix">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/6/69/Netflix_logo.svg" 
+                alt="Netflix" 
+                className={`app-icon ${selectedApp === 'netflix' ? 'selected' : ''}`}
+              />
+            </Link>
+          </div>
+
+          <div onClick={() => handleSelectApp('hbo')}>
+            <Link to="/hbo">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/1/17/HBO_Max_Logo.svg"
+                alt="HBO Max"
+                className={`app-icon ${selectedApp === 'hbo' ? 'selected' : ''}`}
+              />
+            </Link>
+          </div>
+
+          <div onClick={() => handleSelectApp('hulu')}>
+            <Link to="/hulu">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/e/e4/Hulu_Logo.svg"
+                alt="Hulu"
+                className={`app-icon ${selectedApp === 'hulu' ? 'selected' : ''}`}
+              />
+            </Link>
+          </div>
+
+          <div onClick={() => handleSelectApp('prime')}>
+            <Link to="/prime">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/f/f1/Prime_Video.png"
+                alt="Prime Video"
+                className={`app-icon ${selectedApp === 'prime' ? 'selected' : ''}`}
+              />
+            </Link>
+          </div>
+
         </div>
         <Routes>
           <Route path="/:id" element={<Child />} />
